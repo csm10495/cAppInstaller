99ed8ac7ddf84f97ab0eba25a2cc795d2c57e873
Charles Machalow <csm10495@gmail.com>
2024-02-19 09:54:16 -0800

    Fix

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     include_package_data = True,
-    install_requires=['git+https://github.com/csm10495/PySimpleGUI-4-foss', 'numpy'],
+    install_requires=['PySimpleGUI @ git+https://github.com/csm10495/PySimpleGUI-4-foss@unicode-decode-error-workaround', 'numpy'],
 )
